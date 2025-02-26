fc2a44778b4713e2f8562441833cb53fc3136b61
Andrew Abramov <andrabra@yandex.ru>
2025-02-26 17:32:52 +0500

    Review: Добавил имя активного пользователя в хедер

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,17 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../services/slices/userSlice';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const userState = useSelector(selectUser);
+  const userName = userState.user?.name;
+
+  return (
+    <>
+      <AppHeaderUI userName={userName || 'Личный кабинет'} />
+      <Outlet />
+    </>
+  );
+};
