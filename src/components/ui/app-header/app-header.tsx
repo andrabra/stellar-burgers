614b0a75c9042d20c8b9901d2cc56398309e0f57
Andrew Abramov <andrabra@yandex.ru>
2025-02-26 17:41:25 +0500

    Review: Добавил свойство isActive для изменения иконки и стиля пункта меню в хедере

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -1,38 +1,68 @@
 import React, { FC } from 'react';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
+import { NavLink, Link } from 'react-router-dom';
+import clsx from 'clsx';
 import {
   BurgerIcon,
   ListIcon,
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { NavLink } from 'react-router-dom';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <>
-          <BurgerIcon type={'primary'} />
-          <NavLink to='/' className='text text_type_main-default ml-2 mr-10'>
-            Конструктор
-          </NavLink>
-        </>
-        <>
-          <ListIcon type={'primary'} />
-          <NavLink to='/feed' className='text text_type_main-default ml-2'>
-            Лента заказов
-          </NavLink>
-        </>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            clsx(styles.link, isActive && styles.link_active)
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2 mr-10'>
+                Конструктор
+              </p>
+            </>
+          )}
+        </NavLink>
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            clsx(styles.link, isActive && styles.link_active)
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <ListIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>Лента заказов</p>
+            </>
+          )}
+        </NavLink>
       </div>
       <div className={styles.logo}>
-        <Logo className='' />
+        <Link to='/'>
+          <Logo className='' />
+        </Link>
       </div>
       <div className={styles.link_position_last}>
-        <ProfileIcon type={'primary'} />
-        <NavLink to='/profile' className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
+        <NavLink
+          to='/profile'
+          className={({ isActive }) =>
+            clsx(styles.link, isActive && styles.link_active)
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>
+                {userName || 'Личный кабинет'}
+              </p>
+            </>
+          )}
         </NavLink>
       </div>
     </nav>
